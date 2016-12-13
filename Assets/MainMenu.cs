aa170cebd40e01c2debb2502311ceefbdac43097
VALENTIN\valen <valentinbelotserkovskiy@gmail.com>
2016-12-13 16:20:52 +0200

    Localization

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -10,6 +10,16 @@ public class MainMenu : MonoBehaviour {
     public Text SettingButton;
     public Text ExitButton;
 
+    void Start()
+    {
+        DiceButton.text = LangManager.instance.GetWord("Dice");
+        CoinButton.text = LangManager.instance.GetWord("Coin");
+        TimersButton.text = LangManager.instance.GetWord("Timers");
+        TwisterButton.text = LangManager.instance.GetWord("Twister");
+        SettingButton.text = LangManager.instance.GetWord("Settings");
+        ExitButton.text = LangManager.instance.GetWord("Exit");
+    }
+
     public void DiceScene ()
     {SceneManager.LoadScene("DiceScene");}
     public void CoinScene()
