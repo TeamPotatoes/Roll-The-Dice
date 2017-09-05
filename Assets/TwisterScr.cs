21e4a9c643a218dfd5fed16ff349b78495e41780
Gaio26 <gismo0_o@mail.ru>
2017-09-05 23:43:49 +0300

    fixes
    fixes after lang execution

--- Assets/TwisterScr.cs
+++ Assets/TwisterScr.cs
@@ -5,21 +5,14 @@ using UnityEngine.SceneManagement;
 public class TwisterScr : MonoBehaviour {
     private int finalnumber = 0; // результат вращения цвет    
     private int finalnumber2 = 0; // результат вращения конечность 
-    public Text TxtTurn;
     public Text TxtResult;
     public Text TxtResult2;
-    public Text TxtBack;
     public Sprite tw_foot; //спрайт ноги
     public Sprite tw_hand; //спрайт руки
 
     void Start()
     {
-
         GetComponent<Renderer>().material.color = new Color(0.0F, 1.0F, 0.0F, 0.0F); //спрайт ноги виден при работе, но при старте игры станет прозрачным. Удобно
-       // TxtTurn.text = LangManager.instance.GetWord("Turn");
-      /*  TxtResult.text = LangManager.instance.GetWord("TwisterColor");
-        TxtResult2.text = LangManager.instance.GetWord("TwisterLimb");*/
-    //    TxtBack.text = LangManager.instance.GetWord("Back");
     }
     //Функции вызвываемые по клику мышки
     public void ClickFlip()
@@ -28,55 +21,54 @@ public class TwisterScr : MonoBehaviour {
         finalnumber2 = Random.Range(1, 5);
         if (finalnumber == 1)
         {
-           // TxtResult.text = LangManager.instance.GetWord("Red");
+            TxtResult.text = "Red";
             TxtResult.color = Color.red;
             GetComponent<SpriteRenderer>().material.color = Color.red;
         } else if (finalnumber == 2)
         {
-          //  TxtResult.text = LangManager.instance.GetWord("Green");
+            TxtResult.text = "Green";
             TxtResult.color = Color.green;
             GetComponent<SpriteRenderer>().material.color = Color.green;
         }else if (finalnumber == 3)
         {
-           // TxtResult.text = LangManager.instance.GetWord("Yellow");
+            TxtResult.text = "Yellow";
             TxtResult.color = Color.yellow;
             GetComponent<SpriteRenderer>().material.color = Color.yellow;
         }else if (finalnumber == 4)
         {
-           // TxtResult.text = LangManager.instance.GetWord("Blue");
+            TxtResult.text = "Blue";
             TxtResult.color = Color.blue;
             GetComponent<SpriteRenderer>().material.color = Color.blue;
         }
         if (finalnumber2 == 1)
         {
-            //TxtResult2.text = LangManager.instance.GetWord("HandLeft");
+            TxtResult2.text = "Left Hand";
             GetComponent<SpriteRenderer>().sprite = tw_hand;
             GetComponent<SpriteRenderer>().flipX = true;
         } else if (finalnumber2 == 2)
         {
-          //  TxtResult2.text = LangManager.instance.GetWord("HandRight");
+            TxtResult2.text = "Right Hand";
             GetComponent<SpriteRenderer>().sprite = tw_hand;
             GetComponent<SpriteRenderer>().flipX = false;
         } else if (finalnumber2 == 3)
         {
-           // TxtResult2.text = LangManager.instance.GetWord("FootLeft");
+            TxtResult2.text = "Left Foot";
             GetComponent<SpriteRenderer>().sprite = tw_foot;
             GetComponent<SpriteRenderer>().flipX = false;
         } else if (finalnumber2 == 4)
         {
-          //  TxtResult2.text = LangManager.instance.GetWord("FootRight");
+            TxtResult2.text = "Right Foot";
             GetComponent<SpriteRenderer>().sprite = tw_foot;
             GetComponent<SpriteRenderer>().flipX = true;
         }
     }
+
     void Update()
-    {
-      
+    {      
         if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            SceneManager.LoadScene("MainMenu");
-        }
+            { ClickBack();}
     }
+
     public void ClickBack()
     {
         SceneManager.LoadScene("MainMenu");
